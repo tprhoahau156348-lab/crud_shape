59fb78d0b6915bc5b7265fb23654c7f01b7ff71a
Efraim156 <tprhoahau156348@gmail.com>
2026-05-27 15:59:21 +0300

    אלו כל הקבצים עדיין בלי הקוד

---
+++ rectangle.py
@@ -0,0 +1,10 @@
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * (self.width + self.height)
