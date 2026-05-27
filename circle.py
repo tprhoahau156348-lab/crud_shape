92989770765055c81aa7dc2f4412d050bb898e6c
Efraim156 <tprhoahau156348@gmail.com>
2026-05-27 18:02:35 +0300

    הכנסתי את החישוב של העיגול

--- circle.py
+++ circle.py
@@ -1,9 +1,16 @@
 import math
-from turtle import shape
+from calculator import Shape
 
-class Circle(shape):
+class Circle(Shape):
 
     def __init__(self, radius):
-        pass
+        self.radius = radius
+
+    def get_perimeter(self):
+        return 2 * math.pi * self.radius
+
     def get_area(self):
-        pass
+        return math.pi * self.radius ** 2
+
+    def __str__(self):
+        return f"Circle(radius={self.radius})"
