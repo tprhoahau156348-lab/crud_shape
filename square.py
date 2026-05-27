e6934c45040b089663f7d67d2efcba72482bc0ec
Efraim156 <tprhoahau156348@gmail.com>
2026-05-27 18:44:13 +0300

    תיקונים

--- square.py
+++ square.py
@@ -1,7 +1,11 @@
 from rectangle import Rectangle
 
-
 class Square(Rectangle):
-
     def __init__(self, side_length):
         super().__init__(side_length, side_length)
+
+    def __str__(self):
+        return "Shape: Square"
+
+    def __repr__(self):
+        return f"Square(side_length={self.width})"
