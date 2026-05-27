d60d95404f602bff23d2c2c878f2779385ff41e9
Efraim156 <tprhoahau156348@gmail.com>
2026-05-27 18:13:53 +0300

    הכנסתי את החישוב של המשושה

--- hexagon.py
+++ hexagon.py
@@ -1,10 +1,18 @@
 import math
 
+from calculator import Shape
 
-class Hexagon:
+
+class Hexagon(Shape):
 
     def __init__(self, side_length):
         self.side_length = side_length
 
     def get_area(self):
-        pass
+        return (3 * math.sqrt(3) * self.side_length ** 2) / 2
+
+    def get_perimeter(self):
+        return 6 * self.side_length
+
+    def __str__(self):
+        return f"Hexagon(side_length={self.side_length})"
