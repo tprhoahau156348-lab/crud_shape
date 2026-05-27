2da9c7e671723def89c9ac9adb35ff9366d92930
Efraim156 <tprhoahau156348@gmail.com>
2026-05-27 18:40:26 +0300

    תיקונים

--- calculator.py
+++ calculator.py
@@ -11,4 +11,3 @@ class Shape:
     def __repr__(self):
         return f"{self.__class__.__name__}()"
     
-print("f")
--- rectangle.py
+++ rectangle.py
@@ -1,17 +1,24 @@
 from calculator import Shape
 
-
 class Rectangle(Shape):
     def __init__(self, width, height):
+        if not isinstance(width, (int, float)) or not isinstance(height, (int, float)):
+            raise TypeError("Sides must be numbers")
+            
+        if width <= 0 or height <= 0:
+            raise ValueError("Side must be greater than 0")
+
         self.width = width
         self.height = height
 
     def get_area(self):
-        
         return self.width * self.height
     
     def get_perimeter(self):
         return 2 * (self.width + self.height)
     
     def __str__(self):
+        return "Shape: Rectangle"
+
+    def __repr__(self):
         return f"Rectangle(width={self.width}, height={self.height})"
