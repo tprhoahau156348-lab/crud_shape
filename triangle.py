13f2ed73981011c9fb66e6bb65b93497bda6595d
Efraim156 <tprhoahau156348@gmail.com>
2026-05-27 17:50:58 +0300

    הכנסתי את החישוב של המשולש

--- triangle.py
+++ triangle.py
@@ -3,7 +3,12 @@ from rectangle import Rectangle
 class Triangle(Rectangle):
 
     def __init__(self, base, height, side_a, side_b, side_c):
+        self.side_a = side_a
+        self.side_b = side_b
+        self.side_c = side_c
         super().__init__(base, height)
 
     def get_area(self):
-        pass
+        return self.width * self.height / 2
+    def get_perimeter(self):
+        return self.side_a + self.side_b + self.side_c
