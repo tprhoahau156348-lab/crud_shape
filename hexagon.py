d122cbb6848ff3f4c173c918e530a64f0b1003bd
Efraim156 <tprhoahau156348@gmail.com>
2026-05-27 18:47:39 +0300

    תיקונים

--- hexagon.py
+++ hexagon.py
@@ -1,18 +1,25 @@
+# hexagon.py
 import math
-
 from calculator import Shape
 
-
 class Hexagon(Shape):
-
-    def __init__(self, side_length):
-        self.side_length = side_length
+    def __init__(self, side):
+        if not isinstance(side, (int, float)):
+            raise TypeError("Side must be a number")
+            
+        if side <= 0:
+            raise ValueError("Side must be greater than 0")
+            
+        self.side = side
 
     def get_area(self):
-        return (3 * math.sqrt(3) * self.side_length ** 2) / 2
+        return (3 * math.sqrt(3) * (self.side ** 2)) / 2
 
     def get_perimeter(self):
-        return 6 * self.side_length
+        return 6 * self.side
 
     def __str__(self):
-        return f"Hexagon(side_length={self.side_length})"
+        return "Shape: Hexagon"
+
+    def __repr__(self):
+        return f"Hexagon(side={self.side})"
